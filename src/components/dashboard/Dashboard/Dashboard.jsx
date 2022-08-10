f29e7597eac9820dc071c1df23c6dfbb512c5326
Success <ikhinobelesuccess@gmail.com>
2022-08-10 04:31:56 +0100

    updates

--- src/components/dashboard/Dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard/Dashboard.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
 import Sidebar from "./../Sidebar/Sidebar";
-import Content from "../Content/Content";
+import Logo from "../../../assets/images/Logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import Content from "./../Content/Content/Content";
 
 const Dashboard = () => {
   const [activeMenu, setActiveMenu] = useState("menu1");
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className="dashboard">
-      <div className="sidebar">
+      <div className={open ? "sidebar-mobile open" : "sidebar"}>
         <Sidebar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
       </div>
       <div className="content">
         <Content activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
       </div>
+
+      <div className="mobile-view">
+        <img src={Logo} alt="assim-logo" />
+        <MenuIcon className="hamburger" onClick={handleClick} />
+      </div>
+
+      {open && <div className="overlay" />}
     </div>
   );
 };
